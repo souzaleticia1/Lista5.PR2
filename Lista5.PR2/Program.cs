a5df5704cfdf7dd96907f1bf87bb9bb4ee3448e7
CJ3016625 <souza.leticia1@aluno.ifsp.edu.br>
2023-04-12 10:34:19 -0300

    Adicionar arquivos de projeto.

---
+++ Lista5.PR2/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista5PR2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercício 1.
+
+            //int i, n;
+
+            //Console.WriteLine("Digite um valor: ");
+            //n = int.Parse(Console.ReadLine());
+
+            //for (i = 1; i <= n; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadKey();
+
+
+            //Exercíco 2.
+
+            //int i, n;
+
+            //Console.WriteLine("Digite um valor: ");
+            //n = int.Parse(Console.ReadLine());
+
+            //for (i = 1; i <= n; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //Console.ReadKey();
+
+
+            //Exercício 3.
+
+            //int i, n;
+
+            //Console.WriteLine("Digite um valor: ");
+            //n = int.Parse(Console.ReadLine());
+
+            //for (i = 1000; i >= n; i--)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //Console.ReadKey();
+
+
+            //Exercício 4.
+
+            //int s = 0, i;
+
+            //for (; s < 200;)
+            //{
+            //    Console.WriteLine("Digite um número: ");
+            //    i = int.Parse(Console.ReadLine());
+
+            //    if (i > 0)
+            //    {
+            //        s += i;
+            //    }
+            //}
+            //Console.ReadKey();
+
+            //Exercíco 5.
+
+            //int i, n;
+
+            //Console.WriteLine("Digite um valor: ");
+            //n = int.Parse(Console.ReadLine());
+
+            //for(i = 1; i <= n; i++)
+            //{
+            //    if(n %i == 0)
+            //    {
+            //        Console.WriteLine("É divisível por: " + i);
+            //    }
+            //}
+            //Console.ReadKey();
+
+
+            //Exercício 6.
+
+            //int i, n, menor, maior;
+
+            //Console.WriteLine("Digite um valor: ");
+            //n = int.Parse(Console.ReadLine());
+
+            //menor = n;
+            //maior = n;
+
+            //for (i = 0; i < 9; i++)
+            //{
+            //    Console.WriteLine("Digite um valor: ");
+            //    n = int.Parse(Console.ReadLine());
+
+            //    if (n < menor)
+            //    {
+            //        menor = n;
+            //    }
+            //    else
+            //    {
+            //        if (n > maior)
+            //        {
+            //            maior = n;
+            //        }
+            //    }
+            //    Console.WriteLine(menor);
+            //    Console.WriteLine(maior);
+            //}
+            //Console.ReadKey();
+
+
+
+            //Exercício 7.
+
+            //int n = -1, somarPar = 0;
+
+            //for(; n != 0;)
+            //{
+            //    Console.WriteLine("Entre com n: ");
+            //    n = int.Parse(Console.ReadLine());
+
+            //    if (n % 2 == 0)
+            //        somarPar = somarPar + n;
+            //    else
+            //        somarPar += n;
+            //}
+            //Console.WriteLine("Parse: " + somarPar);
+            //Console.WriteLine("Impares: " + somarPar);
+            //Console.ReadKey();
+
+        }
+    }
+}
